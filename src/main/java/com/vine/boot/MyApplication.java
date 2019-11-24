45beed5c2d53d850784dd37401e6c2a2df1f25aa
hk177955 <hk177955@alibaba-inc.com>
2019-11-24 22:38:08 +0800

    initial project

---
+++ src/main/java/com/vine/boot/MyApplication.java
@@ -0,0 +1,16 @@
+package com.vine.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hk
+ * @date 2019-11-24 22:34
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
