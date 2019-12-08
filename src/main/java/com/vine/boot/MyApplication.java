6721d6b45622374c6d5b81b2ae7924bf515934b2
hk177955 <hk177955@alibaba-inc.com>
2019-12-08 20:30:44 +0800

    dev-tools

--- src/main/java/com/vine/boot/MyApplication.java
+++ src/main/java/com/vine/boot/MyApplication.java
@@ -1,7 +1,11 @@
 package com.vine.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author hk
@@ -10,7 +14,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(MyApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+    }
+
+    @PostConstruct
+    public void myLog(){
+        log.trace("trace msg");
+        log.debug("debug msg");
+        log.info("info msg");
+        log.warn("warn msg");
+        log.error("error msg");
     }
 }
